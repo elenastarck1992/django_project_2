f7d758e85cb4bb87fa3ba20ed48e908c646d18f0
elena <elenastarck1992@mail.ru>
2023-11-17 13:23:56 +0300

    выполнены задания 1-3

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name =models.CharField(max_length=100)
+    description = models.TextField
+    image = models.ImageField(upload_to='products_images/', verbose_name='изображение')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price = models.IntegerField
+    date_of_creation = models.DateField
+    date_of_change = models.DateField
 
+
+    def __str__(self):
+        return f'{self.product_name}  {self.price}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural= 'товары'
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    category_description = models.TextField
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_shop',
+        'USER': 'postgres',
+        'PORT': 5433,
+        'PASSWORD': '12345'
     }
 }
 
