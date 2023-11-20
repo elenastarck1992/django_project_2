ed90dd5ef17fd0ec22fee9a6d79d83882df6b350
elena <elenastarck1992@mail.ru>
2023-11-20 09:10:45 +0300

    добавлено поле created_at, создана и применена миграция

--- catalog/models.py
+++ catalog/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
+NULLABLE = {'blank': True, 'null': True}
 class Product(models.Model):
     product_name =models.CharField(max_length=100)
-    description = models.TextField
-    image = models.ImageField(upload_to='products_images/', verbose_name='изображение')
+    description = models.TextField()
+    image = models.ImageField(upload_to='products_images/', verbose_name='изображение', **NULLABLE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    price = models.IntegerField
-    date_of_creation = models.DateField
-    date_of_change = models.DateField
-
+    price = models.IntegerField()
+    date_of_creation = models.DateField()
+    date_of_change = models.DateField()
 
     def __str__(self):
         return f'{self.product_name}  {self.price}'
@@ -19,4 +20,5 @@ class Product(models.Model):
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
-    category_description = models.TextField
+    category_description = models.TextField()
+    created_at = models.TextField()
