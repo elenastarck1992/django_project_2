00b31d5a9b73781bb61cf5f9ee91767ba7870480
elena <elenastarck1992@mail.ru>
2023-11-20 21:55:43 +0300

    создана админка, в нее добавлены фильтрация и поиск

--- catalog/models.py
+++ catalog/models.py
@@ -2,24 +2,33 @@ import datetime
 
 from django.db import models
 
-
 NULLABLE = {'blank': True, 'null': True}
+
+
 class Product(models.Model):
-    product_name =models.CharField(max_length=100)
-    description = models.TextField('')
+    product_name = models.CharField(max_length=100, verbose_name='название товара')
+    description = models.TextField(default='', verbose_name='описание товара')
     image = models.ImageField(upload_to='products_images/', verbose_name='изображение', **NULLABLE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    price = models.IntegerField()
-    date_of_creation = models.DateField(datetime.datetime.now())
-    date_of_change = models.DateField(datetime.datetime.now())
+    price = models.IntegerField(default=0)
+    date_of_creation = models.DateField(default=datetime.datetime.now())
+    date_of_change = models.DateField(default=datetime.datetime.now())
 
     def __str__(self):
         return f'{self.product_name}  {self.price}'
 
     class Meta:
         verbose_name = 'товар'
-        verbose_name_plural= 'товары'
+        verbose_name_plural = 'товары'
+
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100)
-    category_description = models.TextField('')
+    category_name = models.CharField(max_length=100, verbose_name='категория товара')
+    category_description = models.TextField(verbose_name='описание категории', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.category_name}  {self.category_description}'
+
+    class Meta:
+        verbose_name = 'категория товара'
+        verbose_name_plural = 'категории товаров'
